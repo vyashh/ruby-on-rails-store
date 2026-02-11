b91802165c2f79ce301cf41e0436c7b6e3e52f14
Vyash Bhawan <vyashbhawan@gmail.com>
2026-02-11 11:27:09 +0100

    Added rich text area description

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description ])
     end
 end
